203f2f861a683902a640c9a1843a7769c2eb6295
ltr2020 <63619930+ltr2020@users.noreply.github.com>
2021-10-03 14:38:26 +0800

    .

--- W1_library&matrix.py
+++ W1_library&matrix.py
@@ -36,6 +36,7 @@ print(np.array([a[0,0], a[1,1], a[2,1]]))
 print(a[[0,1,2], [0,1,1]])  #all row selected & J10, J11, J21 selected
 print(a>4)  #Boolean indexing
 print(a[a>4])
+print("")
 
 
 #PIL image processing
@@ -49,15 +50,11 @@ print("\n", array.shape)
 print("\n", array)    #200x200 array with all values = unit8 up to bites per byte(2**8=) 256 in size
 array2D = array.reshape(1467,6603)  #3D to 2D
 print("\n", array2D.shape)
-print("\n", array2D)
 mask = np.full(array2D.shape,255) #same shape but all var = 255 (white)
-print("\n", mask)
 modified_array = array2D - mask
 print(modified_array)
 modified_array -= 1 #convert all negative to positive
 modified_array = modified_array.astype(np.uint8)    #tell np to set the value of datatype correctly
-print(modified_array.shape)
-print(modified_array)
 inverted = Image.fromarray(modified_array)
 inverted.show() #B&W inverted image but dimension changed
 
@@ -78,7 +75,7 @@ import csv
 with open(r'Doc/mpg.csv') as csvfile:
     mpg = list(csv.DictReader(csvfile)) #`csv.Dictreader`read each row as a dictionary.
 print("\n")
-print(mpg[:3]) # The first three s = dictionaries in our list
+print(mpg[:3]) # The first three rows = dictionaries in our list
 print(len(mpg))    #234 dictionaries = 234 cars
 print(mpg[1].keys())   #`keys` gives us the column names of our csv
 print(sum(float(i['cty']) for i in mpg) / len(mpg))    #average cty fuel economy across all cars. All values in the dictionaries are strings, so we need to convert to float
@@ -117,27 +114,6 @@ for t in vehicleclass: # iterate over all the vehicle classes
 
 HwyMpgByClass.sort(key=lambda x: x[0])
 print(HwyMpgByClass)
-
-#Another example
-wines = np.genfromtxt('datasets/winequality.red.csv', delimiter=';', skip_header=1)
-#genfromtxt('filename', dtype (optional), delimiter (optional, skip header) to load data
-print(wines)
-print(wines[:, 0]) #first column only
-print(wines[:, 0:1]).mean()    #first column & preserve the form
-
-#Another example
-grad_admission = np.genfromtxt('datasets/Admission_Predict.csv', dtype=None, delimiter=',', skip_header=1, names=('Serial No', 'GRE', 'IELTS', 'Uni Ranking', 'CGPA', 'Research', 'Chance'))
-print(grad_admission.shape) #get dimension
-grad_admission['CGPA'][0:5] #first 5 values of CGPA column
-
-Res_exp = grad_admission[grad_admission['Research'] == 1]
-#Boolean indexing to pull those with research exp
-len(Res)    #No. of those students
-
-print(grad_admission[grad_admission['Chance'] > 0.8]['GRE'].mean())
-#Pull those with 80% chance and get their mean GRE score
-print("")
-
 
 #Regex
 import re
