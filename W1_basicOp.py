70faeb8e9b279030f56dede5432a66daf008c9e7
ltr2020 <63619930+ltr2020@users.noreply.github.com>
2021-10-27 14:12:43 +0800

    .

--- W1_basicOp.py
+++ W1_basicOp.py
@@ -30,7 +30,8 @@ print(
         sales_record['price']))
 
 # Class function
-
+# an outline for creating a new object. An object is anything that you wish to manipulate or change while working through the code.
+# Every time a class object is instantiated, which is when we declare a variable, a new object is initiated from scratch.
 
 class Account:
     def __init__(self, number, name):
